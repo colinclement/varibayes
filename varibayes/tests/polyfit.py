e90d92f39b6a7efa4ffc8da39f60cdadfef00657
Colin Clement <colin.clement@gmail.com>
2017-10-19 22:20:59 -0400

    switched default opt

--- varibayes/tests/polyfit.py
+++ varibayes/tests/polyfit.py
@@ -7,12 +7,12 @@ date: 2017-10-18
 Minimal working example of using VariationalInference to fit polynomials
 """
 
-import sys
-sys.path.append('../')
 import numpy as np
-import varibayes
+import varibayes.infer as infer
+from varibayes.opt.adam import Adam
 
-x = np.linspace(-1,1,400)
+x = np.linspace(-1,1,100)
+rng = np.random.RandomState(92089)
 
 def loglikelihood(params, data, x=x):
     sigma = p[0]
@@ -34,13 +34,18 @@ if __name__=='__main__':
 
     sigma = 0.01  # we divide y by its ptp so sigma is 1/SNR
     p = np.hstack([sigma, pp])
-    data = y + sigma*np.random.randn(len(y))
+    data = y + sigma*rng.randn(len(y))
 
-    vb = varibayes.VariationalInferenceMF(loglikelihood, args=(data,),
-                                          samples=400)
+    vb = infer.VariationalInferenceMF(loglikelihood, args=(data,),
+                                      samples=30)
 
-    p0 = np.hstack([sigma, 0*pp/ptp + 0.01*np.random.randn(len(pp)),
-                    0.0001*np.random.rand(len(p))])
-    vb.fit_rao_blackwell(p0.copy(), iprint=100, tol=1E-7, itn=20000)
-    plt.plot(vb.opt_rao_blackwell.obj_list)
+    p0 = np.hstack([sigma, 0*pp/ptp + 0.1*rng.randn(len(pp)),
+                    0.1*rng.rand(len(p))])
+    vb.fit(p0.copy(), iprint=100, tol=1E-8, itn=15000)
+
+    opt = Adam(vb.evd_grad_evd_rao_blackwell)
+    sol = opt.optimize(p0.copy(), iprint=100, tol=1E-8, itn=15000)
+    plt.plot(vb.opt.obj_list, label='Adagrad')
+    plt.plot(opt.obj_list, label='Adam')
+    plt.legend()
     plt.show()
